e923915f10ac20f1495a22a137aea54e96f0af4f
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2017-05-02 13:26:10 -0400

    Get libraries list

--- main.py
+++ main.py
@@ -24,26 +24,31 @@ def get_enrichr_results(gene_set_library, genelist, description):
     return [data['shortId'], json.loads(response.text)]
 
 
+def get_libraries():
+    libs_json = json.loads(requests.get('http://amp.pharm.mssm.edu/Enrichr/datasetStatistics').text)
+    libs = [lib['libraryName']for lib in libs_json['statistics']]
+    return libs
+
 def main():
-    library = 'ChEA_2016'
-    crisp = open('crisp.txt', 'r').read()
-    data = get_enrichr_results(library, crisp, 'Sample gene list')
-    print('http://amp.pharm.mssm.edu/Enrichr/enrich?dataset={0}'.format(data[0]))
-    # Result fields:
-    # 0 - Index
-    # 1 - Term
-    # 2 - P-value
-    # 3 - Z-score
-    # 4 - Combined score
-    # 5 - Genes
-    # 6 - Adjusted p-value
-    # 7 - Old p-value
-    # 8 - Old adjusted p-value
-    # Old p-value and old adjusted p-value were added for backward compatibility.
-    # In general you don't need them
-    print('Term\tP-Value\tAdjusted p-value\tZ-score\tCombined score\tGenes')
-    for term in data[1][library]:
-        print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(term[1], term[2], term[6], term[3], term[4], '\t'.join(term[5])))
+    for library in get_libraries():
+        crisp = open('test.txt', 'r').read()
+        data = get_enrichr_results(library, crisp, 'Sample gene list')
+        print('http://amp.pharm.mssm.edu/Enrichr/enrich?dataset={0}'.format(data[0]))
+        # Result fields:
+        # 0 - Index
+        # 1 - Term
+        # 2 - P-value
+        # 3 - Z-score
+        # 4 - Combined score
+        # 5 - Genes
+        # 6 - Adjusted p-value
+        # 7 - Old p-value
+        # 8 - Old adjusted p-value
+        # Old p-value and old adjusted p-value were added for backward compatibility.
+        # In general you don't need them
+        print('Term\tP-Value\tAdjusted p-value\tZ-score\tCombined score\tGenes')
+        for term in data[1][library]:
+            print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(term[1], term[2], term[6], term[3], term[4], '\t'.join(term[5])))
     return None
 
 
