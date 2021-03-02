ed5ce5958b2950d450da2ffd7063e5eb5115c22b
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2021-03-02 15:25:56 -0500

    Update for cloud

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from time import sleep
 
 
 def get_enrichr_results(gene_set_library, genelist, description):
-    ADDLIST_URL = 'http://amp.pharm.mssm.edu/Enrichr/addList'
+    ADDLIST_URL = 'https://maayanlab.cloud/Enrichr/addList'
     payload = {
         'list': (None, genelist),
         'description': (None, description)
@@ -16,7 +16,7 @@ def get_enrichr_results(gene_set_library, genelist, description):
     sleep(1)
     data = json.loads(response.text)
 
-    RESULTS_URL = 'http://amp.pharm.mssm.edu/Enrichr/enrich'
+    RESULTS_URL = 'https://maayanlab.cloud/Enrichr/enrich'
     query_string = '?userListId=%s&backgroundType=%s'
     user_list_id = data['userListId']
     response = requests.get(RESULTS_URL + query_string % (user_list_id, gene_set_library))
@@ -25,7 +25,7 @@ def get_enrichr_results(gene_set_library, genelist, description):
 
 
 def get_libraries():
-    libs_json = json.loads(requests.get('http://amp.pharm.mssm.edu/Enrichr/datasetStatistics').text)
+    libs_json = json.loads(requests.get('https://maayanlab.cloud/Enrichr/datasetStatistics').text)
     libs = [lib['libraryName']for lib in libs_json['statistics']]
     return libs
 
@@ -34,22 +34,7 @@ def main():
     libraries = get_libraries()
     for library in libraries:
         data = get_enrichr_results(library, crisp, 'Sample gene list')
-        print('http://amp.pharm.mssm.edu/Enrichr/enrich?dataset={0}'.format(data[0]))
-        # Result fields:
-        # 0 - Index
-        # 1 - Term
-        # 2 - P-value
-        # 3 - Z-score
-        # 4 - Combined score
-        # 5 - Genes
-        # 6 - Adjusted p-value
-        # 7 - Old p-value
-        # 8 - Old adjusted p-value
-        # Old p-value and old adjusted p-value were added for backward compatibility.
-        # In general you don't need them
-        print('Term\tP-Value\tAdjusted p-value\tZ-score\tCombined score\tGenes')
-        for term in data[1][library]:
-            print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(term[1], term[2], term[6], term[3], term[4], '\t'.join(term[5])))
+        print('https://maayanlab.cloud/enrich?dataset={0}'.format(data[0]))
     return None
 
 
